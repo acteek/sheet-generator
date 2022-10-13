5ae3d31293d5307f84f66dfdf009df862e87d6a4
Sergey Ryazanov <sergey.ryazanov@nordigy.ru>
2022-10-13 15:04:41 +0200

    chengr format to  xlsx

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module csv-generator
 
 go 1.18
 
-require github.com/jaswdr/faker v1.15.0
+require (
+	github.com/jaswdr/faker v1.15.0
+	github.com/tealeg/xlsx v1.0.5
+)
--- main.go
+++ main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"encoding/csv"
 	"flag"
 	"github.com/jaswdr/faker"
+	"github.com/tealeg/xlsx"
 	"log"
 	"math/rand"
-	"os"
 	"strconv"
 	"time"
 )
@@ -33,25 +32,32 @@ func main() {
 	flag.Parse()
 
 	unix := time.Now().Unix()
-
 	dateTime := time.Now().Format("02.01.2006T15.04.05")
-	fileName := "Таблица_для_импорта_кандидатов_в_ХантФлоу_" + dateTime
+	fileName := "Таблица_для_импорта_кандидатов_в_ХантФлоу_" + dateTime + ".xlsx"
 	fake := faker.New()
 
-	f, err := os.Create(fileName + ".csv")
+	//if err != nil {
+	//	log.Fatalln("Failed to open file", err)
+	//}
+
+	//w := csv.NewWriter(f)
+
+	//if err := w.Write(firstLine); err != nil {
+	//	log.Fatalln("Failed to write first line", err)
+	//}
+	file := xlsx.NewFile()
+	sheet, err := file.AddSheet("Sheet1")
+	rowMain := sheet.AddRow()
+	rowMain.WriteSlice(&firstLine, len(firstLine))
 	if err != nil {
-		log.Fatalln("Failed to open file", err)
-	}
-
-	w := csv.NewWriter(f)
-
-	if err := w.Write(firstLine); err != nil {
-		log.Fatalln("Failed to write first line", err)
+		log.Fatalln("Create Sheet error:", err)
 	}
 
 	log.Println("Generation is started ...")
 	for i := 0; i < count; i++ {
 		maxTime := int64(rand.Int31n(int32(unix)))
+		row := sheet.AddRow()
+
 		var record = []string{
 			fake.Person().LastName(),
 			fake.Person().FirstName(),
@@ -67,23 +73,29 @@ func main() {
 			fake.Lorem().Sentence(10),
 		}
 
-		if err := w.Write(record); err != nil {
-			log.Fatalln("Failed to write line #"+string(rune(i)), err)
-		}
+		row.WriteSlice(&record, len(record))
+		//if err := w.Write(record); err != nil {
+		//	log.Fatalln("Failed to write line #"+string(rune(i)), err)
+		//}
 		//log.Println(record)
 		//log.Println(time.Now().Format("02.01.2006"))
 
 	}
 
-	defer func(f *os.File, w *csv.Writer) {
-		st, _ := f.Stat()
-		size := strconv.Itoa(int(st.Size() / 1024))
-		log.Printf("Generation finished, lines:%v size:%vKB, file:%v \n", count, size, f.Name())
-		w.Flush()
-		err := f.Close()
-		if err != nil {
-			log.Println("File Close with Error:", err)
+	defer func(f *xlsx.File, name string) {
+		//st, _ := f.Stat()
+		//size := strconv.Itoa(int(st.Size() / 1024))
+
+		if err := f.Save(fileName); err != nil {
+			log.Fatalln("Save file error:", err)
 		}
-	}(f, w)
+
+		log.Printf("Generation finished, lines:%v, file:%v \n", count, fileName)
+		//file.Save(fileName + ".xlsx")
+		//err := f.Close()
+		//if err != nil {
+		//	log.Println("File Close with Error:", err)
+		//}
+	}(file, fileName)
 
 }
